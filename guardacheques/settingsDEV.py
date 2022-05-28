acc7808c9471ba291252a75cd68970f15d3d47bb
O Vitin <vpbohn@gmail.com>
2022-05-28 02:07:35 -0300

    CHANGE IN LOGIN SCREEN

--- guardacheques/settingsDEV.py
+++ guardacheques/settingsDEV.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "cheques.apps.ChequesConfig",
+    "crispy_forms"
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGIN_REDIRECT_URL = "/cheques"
 LOGIN_URL = "/accounts/login"
+
+#django-crispy-forms
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
